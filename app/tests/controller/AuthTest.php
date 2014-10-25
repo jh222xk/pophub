842101f16c8f99a23fe331ccf6724577c451dcb0
Jesper Håkansson <jh222xk@student.lnu.se>
2014-10-25 13:09:14 +0200

    Added authorize test

--- app/tests/controller/AuthTest.php
+++ app/tests/controller/AuthTest.php
@@ -10,8 +10,16 @@ use PopHub\Controller;
 
 class AuthTest extends \PHPUnit_Framework_TestCase {
 
-  public function testCanAuthenticateUSer() {
+  public function testCanAuthenticateUser() {
+    $configMock = $this->getMock('Kagu\Config\Config', null, array(__DIR__."/../../config/app.php"));
 
+    $modelMock = $this->getMock('PopHub\Model\GitHub', null, array($configMock));
+
+    $adapterMock = $this->getMock('PopHub\Model\GitHubAdapter', array('authorize'), array($modelMock));
+    $adapterMock->expects($this->any())->method('authorize');
+
+    $controller = new Controller\Auth($adapterMock);
+    $controller->index();
   }
 
 }
